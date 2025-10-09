3ea00c36d504c55f3a8dc0a24aa6fc1ae2e2cc64
Gautam <gautam@infogain.com>
2025-10-09 08:39:04 +0530

    testimony block added

---
+++ blocks/testimonies/testimonies.js
@@ -0,0 +1,20 @@
+export default function decorate(block) {
+  [...block.children].forEach((row) => {
+    const clientName = row.children[0];
+    const feedback = row.children[1];
+
+    const testimony = document.createElement('div');
+    testimony.className = 'testimony';
+
+    const nameElement = document.createElement('h3');
+    nameElement.className = 'client-name';
+    nameElement.textContent = clientName.textContent;
+
+    const feedbackElement = document.createElement('p');
+    feedbackElement.className = 'client-feedback';
+    feedbackElement.textContent = feedback.textContent;
+
+    testimony.append(nameElement, feedbackElement);
+    row.replaceWith(testimony);
+  });
+}
