0c6c518c063900e0303228e0f0c85e772f0176de
Gautam <gautam@infogain.com>
2025-10-09 08:49:50 +0530

    quotes removed

--- blocks/testimonies/testimonies.js
+++ blocks/testimonies/testimonies.js
@@ -3,15 +3,15 @@ export default function decorate(block) {
     const clientName = row.children[0];
     const feedback = row.children[1];
 
-    const testimony = document.createElement("div");
-    testimony.className = "testimony";
+    const testimony = document.createElement('div');
+    testimony.className = 'testimony';
 
-    const nameElement = document.createElement("h3");
-    nameElement.className = "client-name";
+    const nameElement = document.createElement('h3');
+    nameElement.className = 'client-name';
     nameElement.textContent = clientName.textContent;
 
-    const feedbackElement = document.createElement("p");
-    feedbackElement.className = "client-feedback";
+    const feedbackElement = document.createElement('p');
+    feedbackElement.className = 'client-feedback';
     feedbackElement.textContent = feedback.textContent;
 
     testimony.append(nameElement, feedbackElement);
